f062b22a93494d733cc47dd636976bd2781c3716
TheAlanK <alan.zordenoni@gmail.com>
2021-12-06 00:57:06 -0300

    Password Authentication and JWT Token

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,6 +9,10 @@ namespace API
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+
         
     }
 }
